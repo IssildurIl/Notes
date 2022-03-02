e8a082a9247539c6b519d86c8613ae8a29356eaa
iishutin <iishutin@zuzex.com>
2022-03-02 17:14:41 +0300

    задизайнил mainactivity сделал drawerlayout, поймал баг

--- app/src/main/java/com/learning/notes/MainActivity.kt
+++ app/src/main/java/com/learning/notes/MainActivity.kt
@@ -1,11 +1,71 @@
 package com.learning.notes
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.navOptions
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.learning.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.Toolbar)
+        setupNavigation()
     }
+
+
+    private fun setupNavigation() {
+        navController = findNavController(R.id.fragment)
+        appBarConfiguration = AppBarConfiguration(binding.NavigationView.menu, binding.DrawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        var fragmentIdToLoad: Int? = null
+        binding.NavigationView.setNavigationItemSelectedListener { item ->
+            fragmentIdToLoad = item.itemId
+            binding.DrawerLayout.closeDrawer(GravityCompat.START, true)
+            return@setNavigationItemSelectedListener true
+        }
+
+        binding.DrawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerOpened(drawerView: View) {}
+
+            override fun onDrawerStateChanged(newState: Int) {}
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+
+            override fun onDrawerClosed(drawerView: View) {
+                if (fragmentIdToLoad != null && navController.currentDestination?.id != fragmentIdToLoad) {
+                    val options = navOptions {
+                        launchSingleTop = true
+                        anim {
+                            exit = R.anim.nav_default_exit_anim
+                            enter = R.anim.nav_default_enter_anim
+                            popExit = R.anim.nav_default_pop_exit_anim
+                            popEnter = R.anim.nav_default_pop_enter_anim
+                        }
+                    }
+                    navController.navigate(requireNotNull(fragmentIdToLoad), null, options)
+                }
+            }
+        })
+    }
+
 }
